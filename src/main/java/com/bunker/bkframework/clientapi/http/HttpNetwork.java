76b7e70a0d0b31b49fa9d347325568cfcfd06e8d
광수 <@192.168.0.102>
2018-03-04 23:51:31 +0900

    km

---
+++ src/main/java/com/bunker/bkframework/clientapi/http/HttpNetwork.java
@@ -0,0 +1,33 @@
+package com.bunker.bkframework.clientapi.http;
+
+import com.bunker.bkframework.business.PeerConnection;
+import com.bunker.bkframework.clientapi.NetHandle;
+import com.bunker.bkframework.clientapi.Network;
+
+public class HttpNetwork implements Network<String, String> {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeHandle(NetHandle<String, String> handle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getNextSequence() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PeerConnection<String> getPeerConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
