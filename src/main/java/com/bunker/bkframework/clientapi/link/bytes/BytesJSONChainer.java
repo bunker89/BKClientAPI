1ccc66767ec927a7400df6f46a849ca90b43fd69
WIN7 <bunker.ys89@gmail.com>
2018-03-16 16:37:22 +0900

    sdhdsh

--- src/main/java/com/bunker/bkframework/clientapi/link/bytes/BytesJSONChainer.java
+++ src/main/java/com/bunker/bkframework/clientapi/link/bytes/BytesJSONChainer.java
@@ -5,6 +5,14 @@ import com.bunker.bkframework.clientapi.link.JSONAdapter;
 
 public class BytesJSONChainer extends Chainer<byte[], byte[]> {
 
+	public BytesJSONChainer(boolean connectionOriented) {
+		super(connectionOriented);
+	}
+
+	public BytesJSONChainer(Chainer<byte[], byte[]> parent) {
+		super(parent);
+	}
+
 	public void addChain(JSONAdapter json) {
 		BytesJSONBridgeLink link = new BytesJSONBridgeLink(json);
 		json.setLink(link);
