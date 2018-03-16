7645aed21788044badf29bb487ca9ab6164e0bdb
WIN7 <bunker.ys89@gmail.com>
2018-03-16 16:32:36 +0900

    asg

--- src/main/java/com/bunker/bkframework/clientapi/link/JSONAdapter.java
+++ src/main/java/com/bunker/bkframework/clientapi/link/JSONAdapter.java
@@ -2,7 +2,11 @@ package com.bunker.bkframework.clientapi.link;
 
 import org.json.JSONObject;
 
+import com.bunker.bkframework.clientapi.link.NetLink.OnLinkResultListener;
+
 public abstract class JSONAdapter {
+	private OnLinkResultListener mResult;
+	private String mResultKey;
 
 	@SuppressWarnings("rawtypes")
 	private NetLink mLink;
@@ -11,10 +15,16 @@ public abstract class JSONAdapter {
 	
 	public void setLink(@SuppressWarnings("rawtypes") NetLink link) {
 		mLink = link;
+		mLink.setOnLinkResultListener(mResult, mResultKey);
 	}
 
 	@SuppressWarnings("rawtypes")
 	public NetLink getLink() {
 		return mLink;
+	}
+
+	public void setOnLinkResultListener(OnLinkResultListener listener, String key) {
+		mResult = listener;
+		mResultKey = key;
 	}
 }
